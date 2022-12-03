b2cd5c0085efe2d0ad64d85d0afcfbe109b527ae
diana-garrammone-davinci <115184254+diana-garrammone-davinci@users.noreply.github.com>
2022-12-03 13:42:46 -0300

    Add files via upload
    Tenemos un problema para cargar los equipos que avanzan en el ArrayList de equiposQueAvanzan.

---
+++ TP2/src/TP2/src/Llave.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Llave extends EtapaMundial {
+
+	private ArrayList<Equipo> equiposQueAvanzan;
+
+	public Llave() {
+		super();
+	}
+	
+	public Llave(String descripcionEtapa) {
+        super();
+        this.equiposQueAvanzan = new ArrayList<Equipo>();
+    }
+
+	@Override
+	public ArrayList<Equipo> getEquiposQueAvanzan(){
+		return equiposQueAvanzan;
+	}
+
+	@Override
+	public String toString() {
+		return "Llave [equiposQueAvanzan=" + equiposQueAvanzan + "]";
+	}
+	
+	
+	
+
+}
